a0cf67466ce8fd9f1663fe91a8a69b6d3918f553
HayaMAM <hayamousa0088@gmail.com>
2018-11-08 13:21:58 +0300

    Added Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  
   devise_for :users
-  resources :users, :only => [:show , :index]
-  resources :subjects
-root "pages#home"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:show, :index]
+  resources :subjects, :posts
+  root "pages#home"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
