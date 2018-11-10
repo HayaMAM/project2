b3f8bb31f19d18b32fd2030b5d21f3027331e35a
HayaMAM <hayamousa0088@gmail.com>
2018-11-10 22:14:42 +0300

    fixed the comment problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,25 @@
 class CommentsController < ApplicationController
-    def create
+  def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment.user_id = current_user.id
+    if @comment.save
+    redirect_to @post
+  else
+    flash.now[:danger] = "error"
   end
- 
+  end
+
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
- 
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
